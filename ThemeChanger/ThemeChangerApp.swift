714cc7240650e1e1b1c14cb3ea784a4fa00211d5
Giovanni Ercolano <75883612+giovanni-ercolano@users.noreply.github.com>
2024-01-08 14:34:58 +0100

    Initial Commit

---
+++ ThemeChanger/ThemeChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangerApp.swift
+//  ThemeChanger
+//
+//  Created by Giovanni Ercolano on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
